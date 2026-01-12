6a21177bb999e34ddf98e96615be490cf26c31a0
Haoqin Wei <haoqinw1@uci.edu>
2026-01-12 13:06:10 -0800

    first attempt

--- a0.py
+++ a0.py
@@ -1,2 +1,12 @@
 #this is for assignment 0
 #testing git functionality
+string_1 = "+-+"
+string_2 = "| |"
+string_3 = "-+"
+
+num = int(input("Enter a number: "))
+
+for i in range(num):
+    for j in range(num - 1):
+        print(string_1)
+        print(string_2)
