c5105deebeeb9389b3c340f14194b1b8c140b3e7
Haoqin Wei <haoqinw1@uci.edu>
2026-01-18 23:49:35 -0800

    I m tired

--- assignment1/a1p1.py
+++ assignment1/a1p1.py
@@ -3,14 +3,29 @@ from pathlib import Path
 def main():
     while True:
         user_input = input().strip()
-
         shit = user_input.split()
+
+        if len(shit) == 0:
+            continue
+
         command = shit[0]
 
         if command == "Q":
             break
         elif command == "L":
             print("Got L command")
+            if len(shit) < 2:
+                print("missing directory!")
+                continue
+
+            the_path = shit[1]
+
+            if not the_path.exists():
+                print("ERROR: Directory does not exist")
+                continue
+
+            print(the_path)
+    
         else:
             print("ERROR: Unknown command")
         
