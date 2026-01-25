67d34eda893f727501bd0161b3518cc04cece187
魏公子 <luoweipotter@weigongzis-MacBook-Pro.local>
2026-01-25 15:32:08 -0800

    I add a function that can return how big is the file

--- wp1.py
+++ wp1.py
@@ -1,2 +1,10 @@
 from pathlib import Path
 
+def how_big_are_you(filename):
+    file_path = Path(filename)
+    if file_path.exists:
+        size = file_path.stat().st_size
+        return size
+    
+    else:
+        print("file dose not exist")
