7250bef21c28ee27b0148fed07e16282b73c9121
魏公子 <luoweipotter@weigongzis-MacBook-Pro.local>
2026-01-25 15:56:21 -0800

    add new function and debug, it can read and write

--- wp1.py
+++ wp1.py
@@ -2,9 +2,22 @@ from pathlib import Path
 
 def how_big_are_you(filename):
     file_path = Path(filename)
-    if file_path.exists:
+    if file_path.exists():
         size = file_path.stat().st_size
         return size
     
     else:
         print("file dose not exist")
+
+def reverse_files(ini, output):
+    file_size = how_big_are_you(ini)
+
+    with open(ini, 'rb') as f1:
+        with open(output, 'wb') as f2:
+            for i in range(file_size - 1, -1, -1):
+                f1.seek(i)
+                data = f1.read(1)
+                f2.write(data)
+                
+
+
