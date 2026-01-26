6a70bdbe4d6e232c65f989c29f0f42b584f548e8
魏公子 <luoweipotter@weigongzis-MacBook-Pro.local>
2026-01-25 16:11:31 -0800

    my first draft

--- wp1.py
+++ wp1.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import sys
+
 
 def how_big_are_you(filename):
     file_path = Path(filename)
@@ -9,6 +11,7 @@ def how_big_are_you(filename):
     else:
         print("file dose not exist")
 
+
 def reverse_files(ini, output):
     file_size = how_big_are_you(ini)
 
@@ -18,6 +21,15 @@ def reverse_files(ini, output):
                 f1.seek(i)
                 data = f1.read(1)
                 f2.write(data)
-                
 
 
+def In_N_Out():
+    if len(sys.argv()) < 3:
+        exit
+    inn = sys.argv[1]
+    out = sys.argv[2]
+    reverse_files(inn, out)
+    
+
+if __name__ == "__main__":
+    In_N_Out()
